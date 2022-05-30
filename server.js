4128dd12f9af075c1eb86c27f037308c2352d3e0
Alec Moldovan <alec_moldovan@yahoo.com>
2022-05-30 13:08:49 -0500

    Fxed sors import.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const sort = require('./controllers/sorter.js')
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const cors  require('cors');
+const cors = require('cors');
 
 const app = express();;
 app.use(express.json());
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 const port = 3000;
 
 app.use(cors());
+
 
 app.use(morgan('dev'))
 
